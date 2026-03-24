7e0f0c14ba067a43b77dc3e33d2b695dd3cb3937
David W.H. Swenson <david.swenson@omsf.io>
2026-03-23 20:07:42 -0500

    Slightly better implementation (use external docker provider)

--- modules/lambda-image-republish/versions.tf
+++ modules/lambda-image-republish/versions.tf
@@ -6,9 +6,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-    external = {
-      source  = "hashicorp/external"
-      version = "~> 2.0"
+    # Remove this once the AWS provider is >= 6.19 and can read public ECR
+    # image metadata directly.
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.5"
     }
   }
 }
